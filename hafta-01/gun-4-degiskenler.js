dadb9bc908f070ece24b1dacb6b08c951ef4fb67
dieistowake <159567018+dieistowake@users.noreply.github.com>
2026-07-16 12:15:18 +0300

    hafta-01 g4: değişkenler + tipler + 10 alıştırma

---
+++ hafta-01/gun-4-degiskenler.js
@@ -0,0 +1,49 @@
+const isim = "Mehmet";
+console.log(isim);
+
+const yas = "24";
+console.log(yas);
+
+const ogrenciMi = true;
+console.log(ogrenciMi);
+
+let sehir = "Ankara";
+console.log(sehir);
+sehir = "İstanbul";
+console.log(sehir);
+
+const ulke = "Türkiye";
+console.log(ulke);
+
+const a = 15;
+const b = 25;
+const toplam = a + b;
+console.log(toplam);
+
+const selamlama = "Merhaba" + " " + "Dünya";
+console.log(selamlama);
+
+const cumle = `Benim adım ${isim} ve ${yas} yaşındayım.`;
+console.log(cumle);
+
+console.log(typeof "Merhaba");
+console.log(typeof 42);
+console.log(typeof false);
+
+const birlesim = 10 + "20"; 
+console.log(birlesim);
+console.log(typeof birlesim);
+
+const meslek = "Yazılımcı";
+const yasadigiSehir = "İstanbul";
+
+const kimlikKarti = `
+--- KİMLİK KARTI ---
+İsim: ${isim}
+Yaş: ${yas}
+Meslek: ${meslek}
+Şehir: ${yasadigiSehir}
+--------------------
+`;
+
+console.log(kimlikKarti);
