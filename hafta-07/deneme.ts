d7a19fa93897765310c8dd55d8dee5da0f46b80f
dieistowake <159567018+dieistowake@users.noreply.github.com>
2026-07-22 14:30:32 +0300

    hafta-07 g2: proje typescript'e gecirildi

--- hafta-07/deneme.ts
+++ hafta-07/deneme.ts
@@ -1,5 +1,5 @@
 let isim: string = 'Intern'
-let yas: number = 'yirmi dört'
+let yas: number = 24
 let aktif: boolean = true
 let hobiler: string[] = ['kod', 'kitap', 'kahve']
 let koordinat: [number, number] = [41.0, 29.0] // tuple: sabit uzunluklu, tipleri belli dizi
