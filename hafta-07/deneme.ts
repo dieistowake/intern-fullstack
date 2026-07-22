36fa5197c53586314648fc06b5568ad9130a398c
dieistowake <159567018+dieistowake@users.noreply.github.com>
2026-07-22 14:09:32 +0300

    hafta-07 g1: typescript temelleri denemesi

---
+++ hafta-07/deneme.ts
@@ -0,0 +1,7 @@
+let isim: string = 'Intern'
+let yas: number = 'yirmi dört'
+let aktif: boolean = true
+let hobiler: string[] = ['kod', 'kitap', 'kahve']
+let koordinat: [number, number] = [41.0, 29.0] // tuple: sabit uzunluklu, tipleri belli dizi
+
+let kullanici: { isim: string; yas: number } = { isim: 'Intern', yas: 24 }
